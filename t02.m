24a771341fc56fbccab3f1c5321891610d6016c3
CANALE CHOMBO <ie696846@iteso.mx>
2019-06-03 13:07:51 -0500

    recordandodePDS

--- t02.m
+++ t02.m
@@ -15,25 +15,31 @@ maxy = max(y)
 % play(audio);
 % pause
 
-% 14, 10, 8 y 4
+% N puede tomar los siguientes valores
+% 2^14, 2^10, 2^8, 2^4
+% 16384, 1024, 256, 16
+N = 16;
+% paso de cuantización -> delta
+A = 1;
+delta = 2*A / N;
+% muestra cuantizada -> n
+xq = delta*(round(y/delta));
 
-% audiowrite('TheLovecats8.wav',y,Fs,'BitsPerSample',8);
+% audiowrite('TheLovecats8.wav',xq,Fs,'BitsPerSample',8);
 % [y, Fs] = audioread('TheLovecats8.wav');
 % audio = audioplayer(y,Fs);
 % play(audio);
 % info = audioinfo('TheLovecats8.wav')
 % b = 16;
-version = 2;
-if (1 == version)
-    % % % version sin pasar a bits  % Cuantizar a entero
-    b = 4;
-    swing = (2^b-1)/2 %14->8191.5 %10->512 %8->128 %4->8
-    xq_i    nt =(y*swing);
-    max(xq_int)
-    min(xq_int)
-    class(xq_int)
-    B = rescale(xq_int,-1,1)
-end
+
+% % % version sin pasar a bits  % Cuantizar a entero
+b = 4;
+swing = (2^b-1)/2 %14->8191.5 %10->512 %8->128 %4->8
+xq_i    nt =(y*swing);
+max(xq_int)
+min(xq_int)
+class(xq_int)
+B = rescale(xq_int,-1,1)
 
 % %  https://la.mathworks.com/matlabcentral/answers/48153-encoding-the-signal-with-14-bits
 % D = dec2bin( (xq_int), 14)
