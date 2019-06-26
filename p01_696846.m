19e7ff4022c322870b3012e4669d12e0b5522b09
acch2016 <ie696846@iteso.mx>
2019-06-25 19:15:08 -0500

    p01_parte1done

--- p01_696846.m
+++ p01_696846.m
@@ -12,7 +12,7 @@ info = audioinfo('sippur.wav')
 yizq = y(:,1);
 yder = y(:,2);
 y = (yizq+yder)/2; %convertir el audio a mono
-
+subplot(2,1,1); pwelch(y,[],[],[],Fs,'power'); title('PSD of original signal');
 % audio = audioplayer(y,Fs);
 % play(audio);
 %% filtrar la señal a 15kHz
@@ -21,12 +21,13 @@ o = 100;
 B = 15e3;
 fc = B/(Fs/2);
 f = [0 fc fc 1];
-fil = fir2(o,f,m);
+LPF = fir2(o,f,m);
 % wvtool(fil)
 % fvtool(fil)
-y = filter(fil,1,y);
-% miny = min(y)
-% maxy = max(y)
+y = filter(LPF,1,y);
+subplot(2,1,2); pwelch(y,[],[],[],Fs,'power'); title('PSD of filtered signal');
+miny = min(y)
+maxy = max(y)
 % audio = audioplayer(y,Fs);
 % play(audio);
 % numel(y)
@@ -35,20 +36,26 @@ y = filter(fil,1,y);
 py = sum(y.^2)/numel(y)
 y = y/sqrt(py);
 % py = sum(y.^2)/numel(y)
+% soundsc(y,Fs);
+% play(audio);
 %% Encontrar la potencia del ruido a la salida del filtro receptor, para N0 = 1/(15000*10^(0:0.3:3))
 step = 0:0.3:3;
 N0 = 1./(B*10.^step);
 PN = B.*N0;
 SNR = 1./PN;
 SNRdB = 10*log10(SNR);
+yorig = y;
 for i = 1:numel(PN)
     Noise = sqrt(PN(i))*randn(1,numel(y));
 %     minN = min(Noise)
 %     maxN = max(Noise)
-    y = y+Noise';
-    y = filter(fil,1,y);
-    y = rescale(y,-1,1);
+    y = yorig+Noise';
+    y = filter(LPF,1,y);
+    y = y./max(abs(y));
+%     y = rescale(y,-1,1);
     audiowrite(['/home/acc/Documents/MATLAB/ComunicacionesDigitales/sippur', num2str(round(SNRdB(i))), '.wav'], y, Fs,'BitsPerSample',16);
 end
 % numel(Noise)
-% y = rescale(y,-1,1);
+%%
+fileID = fopen('/home/acc/Documents/MATLAB/ComunicacionesDigitales/sippur.wav')
+file = fread(fileID)
